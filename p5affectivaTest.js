e227966e76f45bb190e7bdc697d5cb0a89598582
Golan Levin <golan@flong.com>
2017-11-12 19:10:24 -0500

    added documentation of Affectiva API fields

--- p5affectivaTest.js
+++ p5affectivaTest.js
@@ -1,4 +1,44 @@
 // p5.js test program to load/display Affectiva API JSON data
+// This program loads and displays the landmark_points.
+// Affectiva version 1.2.827, returns data including the fields: 
+/*
+    "lip suck": 0.0003, 
+    "mean face luminance": 147.039, 
+    "lip stretch": 0.0022, 
+    "anger": 0.0951, 
+    "brow furrow": 95.6861, 
+    "pitch": -9.6267, 
+    "upper lip raise": 42.272, 
+    "chin raise": 0.0002, 
+    "eye closure": 0.0, 
+    "lid tighten": 61.3131, 
+    "contempt": 0.001, 
+    "landmark_points":[],
+    "eye widen": 0.2019, 
+    "smirk": 0.0006, 
+    "yaw": 10.2923, 
+    "engagement": 99.8884, 
+    "cheek raise": 30.5556, 
+    "disgust": 96.857, 
+    "dimpler": 0.0003, 
+    "smile": 94.7677, 
+    "lip press": 0.0003, 
+    "roll": -59.0305, 
+    "joy": 53.5897, 
+    "attention": 91.2259, 
+    "brow raise": 0.0, 
+    "nose wrinkle": 93.4104, 
+    "surprise": 0.0544, 
+    "fear": 0.1192, 
+    "interocular distance": 42.9567, 
+    "jaw drop": 99.3293, 
+    "inner brow raise": 0.0006, 
+    "lip corner depressor": 0.0, 
+    "lip pucker": 0.0, 
+    "sadness": 0.0021, 
+    "valence": 0.0, 
+    "mouth open": 99.4722
+*/
 
 var img;
 var affectivaData;
